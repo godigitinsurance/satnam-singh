e4b8db1a751d53f38cff8c45d326aa416a288d64
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-05-24 05:39:58 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Satnam Singh");
+$("#policyNumber").text("SJ21072820");
+$("#startDate").text("18-May-2020");
+$("#endDate").text("17-May-2021");
+
+
+$("#type").text("Four Wheeler");
+$("#registrationNumber").text("PB009Y1062");
+$("#make").text("MARUTI");
+$("#model").text("ALTO");
+$("#vehicalSubType").text("800 LXI");
+$("#engineNo").text("5355429");
+$("#chassis").text("MA3EUA61S00572999");
